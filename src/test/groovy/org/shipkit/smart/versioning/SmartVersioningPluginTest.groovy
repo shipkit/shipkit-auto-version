bbad7894b924020695fdaefc04f35097f6eeaf11
Szczepan Faber <szczepiq@gmail.com>
2020-01-15 13:43:22 -0800

    Introduced spock for testing

---
+++ src/test/groovy/org/shipkit/smart/versioning/SmartVersioningPluginTest.groovy
@@ -0,0 +1,11 @@
+package org.shipkit.smart.versioning
+
+import spock.lang.Specification
+
+class SmartVersioningPluginTest extends Specification {
+
+    def test() {
+        expect:
+        true
+    }
+}
