847dc6297d4e30ac4e42b2e6829d31691dfa71ef
Szczepan Faber <szczepiq@gmail.com>
2020-07-12 11:47:10 -0700

    Simplified tag push script

--- gradle/push-tag.sh
+++ gradle/push-tag.sh
@@ -2,12 +2,10 @@
 
 # Git push is implemented in the script to make sure we are not leaking GH key to the output
 # Expects:
-# - tag name as a paramter of this script
+# - tag name as a parameter of this script
 
+set -e
 TAG_NAME=$1
 
 echo "Running git push"
-git push https://@github.com/shipkit/org.shipkit.shipkit-auto-version.git $TAG_NAME
-EXIT_CODE=$?
-echo "'git push --quiet' exit code: $EXIT_CODE"
-exit $EXIT_CODE
+git push origin $TAG_NAME
