11543d7fff61728c06d28ac3d67d65c2bfcdb59f
John Plaisted <jplaisted@linkedin.com>
2020-11-12 12:25:52 -0800

    refactor for testability

--- src/main/java/org/shipkit/auto/version/AutoVersionPlugin.java
+++ src/main/java/org/shipkit/auto/version/AutoVersionPlugin.java
@@ -3,19 +3,14 @@ package org.shipkit.auto.version;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
-
 /**
  * The plugin, ideally with zero business logic, but only the Gradle integration code
  */
 public class AutoVersionPlugin implements Plugin<Project> {
 
-  public void apply(Project project) {
-    DeductedVersion version = new AutoVersion(project.getProjectDir()).deductVersion();
-    if (project.getVersion().equals(Project.DEFAULT_VERSION)) {
-      project.allprojects(p -> p.setVersion(version.getVersion()));
+    public void apply(Project project) {
+        DeductedVersion version = new AutoVersion(project.getProjectDir()).deductVersion(project.getVersion().toString());
+        project.allprojects(p -> p.setVersion(version.getVersion()));
+        project.getExtensions().getExtraProperties().set("shipkit-auto-version.previous-version", version.getPreviousVersion());
     }
-    project.getExtensions()
-        .getExtraProperties()
-        .set("shipkit-auto-version.previous-version", version.getPreviousVersion());
-  }
 }
--- src/test/groovy/org/shipkit/auto/version/AutoVersionTest.groovy
+++ src/test/groovy/org/shipkit/auto/version/AutoVersionTest.groovy
@@ -1,5 +1,7 @@
 package org.shipkit.auto.version
 
+
+import org.gradle.api.Project
 import org.gradle.api.logging.Logger
 
 class AutoVersionTest extends TmpFolderSpecification {
@@ -18,7 +20,7 @@ class AutoVersionTest extends TmpFolderSpecification {
         versionFile << "version=1.5.0-SNAPSHOT"
 
         when:
-        def v = autoVersion.deductVersion(log)
+        def v = autoVersion.deductVersion(log, Project.DEFAULT_VERSION)
 
         then:
         v.version == "1.5.0-SNAPSHOT"
@@ -35,7 +37,7 @@ v1.0.1
 """
 
         when:
-        def v = autoVersion.deductVersion(log)
+        def v = autoVersion.deductVersion(log, Project.DEFAULT_VERSION)
 
         then:
         v.version == "1.1.0"
@@ -53,7 +55,7 @@ some commit #2
 """
 
         when:
-        def v = autoVersion.deductVersion(log)
+        def v = autoVersion.deductVersion(log, Project.DEFAULT_VERSION)
 
         then:
         v.version == "2.0.2"
@@ -66,7 +68,7 @@ some commit #2
         versionFile << "version=1.0.*"
 
         when:
-        def v = autoVersion.deductVersion(log)
+        def v = autoVersion.deductVersion(log, Project.DEFAULT_VERSION)
 
         then:
         v.version == "1.0.unspecified"
@@ -75,5 +77,23 @@ some commit #2
         1 * log.lifecycle("Building version '1.0.unspecified'\n" +
                 "  - reason: shipkit-auto-version caught an exception, falling back to reasonable default\n" +
                 "  - run with --debug for more info")
+    }
+
+    def "uses project version if not default"() {
+        versionFile << "version=2.0.*"
+        runner.run("git", "tag") >> "v2.0.0"
+        runner.run("git", "log", "--pretty=oneline", "v2.0.0..HEAD") >> """
+some commit #1
+some commit #2
+"""
+
+        when:
+        def v = autoVersion.deductVersion(log, "2.0.5-SNAPSHOT")
+
+        then:
+        v.version == "2.0.5-SNAPSHOT"
+        v.previousVersion == null
+        1 * log.lifecycle("Building version '2.0.5-SNAPSHOT'\n" +
+            "  - reason: shipkit-auto-version Version was already defined on project.")
     }
 }
